f1b53abd3c3ced6c7844b472f503c812a558b859
sonnguyenan <nguyenanson20001100@gmail.com>
2023-10-30 17:02:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/hero";
+import Headlinecards from "./components/headlinecards/headlinecards";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero />
+      <Headlinecards />
     </div>
   );
 }
---
+++ src/components/headlinecards/headlinecards.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+const Headlinecards = () => {
+    return (
+        <div className=' max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+            {/* Card */}
+            <div className=' rounded-xl relative'>
+                {/* Overlay */}
+                <div className=' absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className=" font-bold text-2xl px-2 pt-4"> New Restaurant</p>
+                    <p className='px-2'>Added Daily</p>
+                    <button className=' border-white bg-white text-black mx-2 absolute bottom-4 py-2 px-4 rounded '>Order Now</button>
+                </div>
+                <img className=' w-full max-h-[160px] md:max-h-[200px] rounded-xl  object-cover' src='https://images.pexels.com/photos/1552635/pexels-photo-1552635.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+
+            </div>
+
+            {/* Card */}
+            <div className=' rounded-xl relative'>
+                {/* Overlay */}
+                <div className=' absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className=" font-bold text-2xl px-2 pt-4"> We Deliver Deserts Too</p>
+                    <p className='px-2'>Tasty Treats</p>
+                    <button className=' border-white bg-white text-black mx-2 absolute bottom-4 py-2 px-4 rounded '>Order Now</button>
+                </div>
+                <img className=' w-full max-h-[160px] md:max-h-[200px] rounded-xl  object-cover' src='https://images.pexels.com/photos/7142957/pexels-photo-7142957.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+
+            </div>
+
+            {/* Card */}
+            <div className=' rounded-xl relative'>
+                {/* Overlay */}
+                <div className=' absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className=" font-bold text-2xl px-2 pt-4"> New Restaurant</p>
+                    <p className='px-2'>Added Daily</p>
+                    <button className=' border-white bg-white text-black mx-2 absolute bottom-4 py-2 px-4 rounded '>Order Now</button>
+                </div>
+                <img className=' w-full max-h-[160px] md:max-h-[200px] rounded-xl  object-cover' src='https://images.pexels.com/photos/3731423/pexels-photo-3731423.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+
+            </div>
+
+            {/* Card */}
+            <div className=' rounded-xl relative'>
+                {/* Overlay */}
+                <div className=' absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className=" font-bold text-2xl px-2 pt-4">We Deliver Deserts Too</p>
+                    <p className='px-2'>Tasty Treats</p>
+                    <button className=' border-white bg-white text-black mx-2 absolute bottom-4 py-2 px-4 rounded'>Order Now</button>
+                </div>
+                <img className=' w-full max-h-[160px] md:max-h-[200px] rounded-xl  object-cover' src='https://images.pexels.com/photos/166451/pexels-photo-166451.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+
+            </div>
+
+            {/* Card */}
+            <div className=' rounded-xl relative'>
+                {/* Overlay */}
+                <div className=' absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className=" font-bold text-2xl px-2 pt-4"> New Restaurant</p>
+                    <p className='px-2'>Added Daily</p>
+                    <button className=' border-white bg-white text-black mx-2 absolute bottom-4 py-2 px-4 rounded '>Order Now</button>
+                </div>
+                <img className=' w-full max-h-[160px] md:max-h-[200px] rounded-xl  object-cover' src='https://images.pexels.com/photos/15832884/pexels-photo-15832884/free-photo-of-mon-an-pizza-tay-b-a-t-i.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+
+            </div>
+
+            {/* Card */}
+            <div className=' rounded-xl relative'>
+                {/* Overlay */}
+                <div className=' absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className=" font-bold text-2xl px-2 pt-4">We Deliver Deserts Too</p>
+                    <p className='px-2'>Tasty Treats</p>
+                    <button className=' border-white bg-white text-black mx-2 absolute bottom-4 py-2 px-4 rounded '>Order Now</button>
+                </div>
+                <img className=' w-full max-h-[160px] md:max-h-[200px] rounded-xl  object-cover' src='https://images.pexels.com/photos/2260200/pexels-photo-2260200.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Headlinecards
